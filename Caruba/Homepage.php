0490ff1f08f85909078767229981d3a19b108ba4
Dustin-Robbins <dustinprobbins@yahoo.com>
2016-12-06 13:26:43 -0500

    Front End
    Here's the front-end as is. So far I've only got the background done, just figuring out how to make the whole thing an image with a color overlay. If you want the color changed that can happen, but I'm going to start work on the navbars now.

---
+++ Caruba/Homepage.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+   
+    <!-- Reset everything -->
+    <link href="reset.css" rel="stylesheet">
+
+    <!-- Get that Bootstrap included -->
+    <link href="bootstrap3_defaultTheme/dist/css/bootstrap.css" rel="stylesheet">
+    <link href="bootstrap3_defaultTheme/theme.css" rel="stylesheet">
+
+    <!-- Get my stylesheet included -->
+    <link href="homepage_styling.css" rel="stylesheet">
+  </head>
+
+  <body>
+  </body>
+</html>
