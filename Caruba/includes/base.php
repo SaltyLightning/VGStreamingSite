6267607aa9d37b32bf57af85159dfa4205efe81f
Dustin-Robbins <dustinprobbins@yahoo.com>
2016-12-07 21:16:11 -0500

    Pretty much done with the bars
    Added in icons, the logo, and recolored the submit button so it doesn't look awful.

---
+++ Caruba/includes/base.php
@@ -0,0 +1,32 @@
+<!-- Get my stylesheet included -->
+<link href="base_styling.css" rel="stylesheet">
+
+<!-- Here's the topbar -->
+<div class="topnav">
+	<a href="homepage.php"><img src="images/logos/logo-2.png" width="60" height="60"></a>
+
+	<!-- <ul>
+		<li><a href="#home">Home</a></li>
+		<li><a href="#news">News</a></li>
+		<li><a href="#contact">Contact</a></li>
+		<li><a href="#about">About</a></li>
+	</ul> -->
+
+	<div class="search">
+		<!-- SEARCH &nbsp; -->
+		<form action="" method="GET">
+			<input type="text" name="query" value=" Search" onfocus="if (this.value==' Search') this.value='';"/>
+			<input type="submit"/>
+		</form>
+	</div>
+	</div>
+
+	<!-- Aaand here's the sidebar -->
+	<div class="sidenav">
+	<ul>
+		<li><a href="#games"><img src="images/icons/gamepad.png"></a></li>
+		<li><a href="#channels"><img src="images/icons/users.png"></a></li>
+		<li><a href="#favorites"><img src="images/icons/favorite.png"></a></li>
+		<li><a href="#settings"><img src="images/icons/control-panel.png"></a></li>
+	</ul>
+</div>
